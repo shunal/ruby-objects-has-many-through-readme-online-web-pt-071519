9f74bd89da1ea05107a2c6dd04c96c4298f10af1
Steven Hunal <stevehunal@gmail.com>
2019-08-09 01:16:10 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,16 +1,18 @@
 class Customer
-  attr_accessor :name, :age
- 
+  attr_accessor :name, :age 
+  
   @@all = []
- 
+  
   def initialize(name, age)
-    @name = name
-    @age = age
-    @@all << self
-  end
- 
-  def self.all
-    @@all
-  end
- 
+    @name = name 
+    @age = age 
+    @all << self
+  end 
+
+  def self.all 
+   @@all 
+  end 
+
+
+
 end
